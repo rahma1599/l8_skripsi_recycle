0a12fbb17251d526c2e91d6722f74580b3fd2c89
wafasa <wafasa@gmail.com>
2022-09-08 22:43:59 +0700

    fixing 08-09-2022

--- resources/views/admin/sidemenu.blade.php
+++ resources/views/admin/sidemenu.blade.php
@@ -4,14 +4,17 @@
       <li class=" navigation-header">
         <span data-i18n="nav.category.layouts">Menu Admin</span><i class="la la-ellipsis-h ft-minus" data-toggle="tooltip" data-placement="right" data-original-title="Layouts"></i>
       </li>
-      <li class=" active"><a href="#"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.templates.main">Dashboard</span></a></li>
-      <li class=" active"><a href="#"><i class="la la-book"></i><span class="menu-title" data-i18n="nav.dash.main">Manage Recycle</span></a>
+      <li class=" active"><a href="#"><i class="la la-users"></i><span class="menu-title" data-i18n="nav.dash.main">Manage Pengguna</span></a>
         <ul class="menu-content">
-          <li class="menu-item"><a class="menu-item" href="{{ route('pengguna.index') }}" data-i18n="nav.dash.ecommerce">Data Recyle</a></li>
-          <li class="menu-item"><a class="menu-item" href="{{ route('admin.dashboard') }}" data-i18n="nav.dash.ecommerce">Input Data Recycle</a></li>
+          <li class="menu-item"><a class="menu-item" href="{{ route('admin.dashboard') }}" data-i18n="nav.dash.ecommerce">Dasboard</a>
+          </li>
+          <li class="menu-item"><a class="menu-item" href="{{ route('pengguna.index') }}" data-i18n="nav.dash.ecommerce">Pengguna</a>
+          </li>
+          <li><a class="menu-item" href="{{ route('pengguna.create') }}" data-i18n="nav.dash.crypto">Tambah Pengguna</a>
           </li>
         </ul>
-      </li>
+    </li>
+      <li class=" active"><a href="#"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.templates.main">Dashboard</span></a></li>
       <li class=" active"><a href="#"><i class="la la-book"></i><span class="menu-title" data-i18n="nav.dash.main">Manage Barang</span></a>
         <ul class="menu-content">
           <li class="menu-item"><a class="menu-item" href="{{ route('admin.dashboard') }}" data-i18n="nav.dash.ecommerce">Data Barang </a></li>
@@ -24,16 +27,14 @@
           <li class="menu-item"><a class="menu-item" href="{{ route('pengguna.index') }}" data-i18n="nav.dash.ecommerce">Input Size</a></li>
         </ul>
       </li>
-    <li class=" active"><a href="#"><i class="la la-users"></i><span class="menu-title" data-i18n="nav.dash.main">Manage Pengguna</span></a>
+      <li class=" active"><a href="#"><i class="la la-book"></i><span class="menu-title" data-i18n="nav.dash.main">Manage Recycle</span></a>
         <ul class="menu-content">
-          <li class="menu-item"><a class="menu-item" href="{{ route('admin.dashboard') }}" data-i18n="nav.dash.ecommerce">Dasboard</a>
-          </li>
-          <li class="menu-item"><a class="menu-item" href="{{ route('pengguna.index') }}" data-i18n="nav.dash.ecommerce">Pengguna</a>
-          </li>
-          <li><a class="menu-item" href="{{ route('pengguna.create') }}" data-i18n="nav.dash.crypto">Tambah Pengguna</a>
+          <li class="menu-item"><a class="menu-item" href="{{ route('pengguna.index') }}" data-i18n="nav.dash.ecommerce">Data Recyle</a></li>
+          <li class="menu-item"><a class="menu-item" href="{{ route('admin.dashboard') }}" data-i18n="nav.dash.ecommerce">Input Data Recycle</a></li>
           </li>
         </ul>
-    </li>
+      </li>
+
     <li class=" active"><a href="#"><i class="la la-users"></i><span class="menu-title" data-i18n="nav.dash.main">Laporan</span></a>
         <ul class="menu-content">
           <li class="menu-item"><a class="menu-item" href="{{ route('admin.dashboard') }}" data-i18n="nav.dash.ecommerce">Belum Potong</a>
--- resources/views/supervisor/sidemenu.blade.php
+++ resources/views/supervisor/sidemenu.blade.php
@@ -7,8 +7,16 @@
       </li>
       <li class="active"><a href="#"><i class="la la-home"></i><span class="menu-title" data-i18n="nav.templates.main">Dashboard</span></a>
       </li>
-      <li class="active"><a href="#"><i class="la la-print"></i><span class="menu-title" data-i18n="nav.scrumboard.main">Laporan Data Recyle</span></a>
-      </li>
+      <li class=" active"><a href="#"><i class="la la-print"></i><span class="menu-title" data-i18n="nav.dash.main">Laporan Data Recyle</span></a>
+        <ul class="menu-content">
+          <li class="menu-item"><a class="menu-item" href="{{ route('admin.dashboard') }}" data-i18n="nav.dash.ecommerce">Belum Potong</a>
+          </li>
+          <li class="menu-item"><a class="menu-item" href="{{ route('pengguna.index') }}" data-i18n="nav.dash.ecommerce">Tunggu Buffing</a>
+          </li>
+          <li><a class="menu-item" href="{{ route('pengguna.create') }}" data-i18n="nav.dash.crypto">Siap Pouring</a>
+          </li>
+        </ul>
+    </li>
       <li class="active"><a href="{{ route('logout') }}"><i class="la la-external-link"></i><span class="menu-title" data-i18n="nav.scrumboard.main">Logout</span></a>
       </li>
     </ul>
