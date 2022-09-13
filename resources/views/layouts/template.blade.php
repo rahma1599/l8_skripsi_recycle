73cd52ae056d358f6e8415686f934ec3d65d4abd
wafasa <wafasa@gmail.com>
2022-09-13 16:05:33 +0700

    change favicon chingluh

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,8 +4,6 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-  <meta name="description" content="Modern admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities with bitcoin dashboard.">
-  <meta name="keywords" content="admin template, modern admin template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
   <meta name="author" content="{{ config('app.name') }}">
   <title>{{ config('app.name') }} - Page</title>
   <link rel="apple-touch-icon" href="{{ asset('modernadmin/app-assets/images/ico/apple-icon-120.png') }}">
